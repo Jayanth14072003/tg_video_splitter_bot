d221ef10083314db8ebcc4370e1c0ed60afe7977
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-04-22 14:13:18 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("BOT_TOKEN","1834175795:AAEx-ebvrPMGhiqSU5PFJwXdnf0ioIb71As")
 
 # For webhook only
 SECRET_TOKEN = os.environ.get("SECRET_TOKEN")
