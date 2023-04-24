e627fe3bd843d010053057a52b1eff4c754c030f
jayanna G <84214745+Jayanth14072003@users.noreply.github.com>
2023-04-24 23:06:01 +0530

    Update settings.py

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ import os
 BOT_TOKEN = os.environ.get("BOT_TOKEN","1834175795:AAEx-ebvrPMGhiqSU5PFJwXdnf0ioIb71As")
 
 # For webhook only
-SECRET_TOKEN = os.environ.get("SECRET_TOKEN")
+SECRET_TOKEN = os.environ.get("SECRET_TOKEN","11xx^^$31ja23dufuxjdj")
 
 # 30 second video split size by default
 SPLIT_SIZE = 30
